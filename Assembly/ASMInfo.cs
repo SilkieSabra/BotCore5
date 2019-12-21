453f69dae9fff042be9e881135337bf8f8bb24b7
Tara <tarapiccari@gmail.com>
2019-12-20 20:06:40 -0700

    Create ASMInfo.cs

---
+++ Assembly/ASMInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Assemble
+{
+    public class ASMInfo
+    {
+        public static string BotName = "ZBotCore";
+        public static double BotVer = 5.1;
+        public static string GitPassword = "**REMOVED**";
+    }
+}
