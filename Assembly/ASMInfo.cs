dde76b54bbd9b947cea6f315a810fb53afd8b9e2
Zontreck <tarapiccari@gmail.com>
2020-03-10 16:20:07 -0700

    update ASM version

--- Assembly/ASMInfo.cs
+++ Assembly/ASMInfo.cs
@@ -7,7 +7,7 @@ namespace Bot.Assemble
     public class ASMInfo
     {
         public static string BotName = "ZBotCore";
-        public static double BotVer = 5.331;
+        public static double BotVer = 5.334;
         public static string GitPassword
         {
             get
