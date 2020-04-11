5c2b544cbd80e5ce0a8ee2feb358fe07766783b0
Zontreck <tara@zontreck.dev>
2020-04-10 17:08:20 -0700

    update stuffs

--- Assembly/ASMInfo.cs
+++ Assembly/ASMInfo.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Reflection;
+
+[assembly: AssemblyCompany("ZNI")]
+[assembly: AssemblyAlgorithmId(System.Configuration.Assemblies.AssemblyHashAlgorithm.MD5)]
+[assembly: AssemblyCopyright("© 2020 Tara Piccari")]
+[assembly: AssemblyFileVersion("5.401")]
+[assembly: AssemblyDescription("Second Life Bot - BotCore5")]
+
 
 namespace Bot.Assemble
 {
     public class ASMInfo
     {
         public static string BotName = "ZBotCore";
-        public static double BotVer = 5.334;
+        public static double BotVer = 5.401;
         public static string GitPassword
         {
             get
