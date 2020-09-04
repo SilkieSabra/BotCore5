426089c654f8a683b2d83675fad1f445548f74f0
Zontreck <tara@zontreck.dev>
2020-09-04 14:08:27 -0700

    update version for force update

--- Assembly/ASMInfo.cs
+++ Assembly/ASMInfo.cs
@@ -7,7 +7,7 @@ using LibZNI;
 [assembly: AssemblyCompany("ZNI")]
 [assembly: AssemblyAlgorithmId(System.Configuration.Assemblies.AssemblyHashAlgorithm.MD5)]
 [assembly: AssemblyCopyright("(C) 2020 Tara Piccari")]
-[assembly: AssemblyFileVersion("5.0.5.1250")]
+[assembly: AssemblyFileVersion("5.0.5.1251")]
 [assembly: AssemblyDescription("Second Life Bot - BotCore5")]
 [assembly: AutoUpdater("/job/ZBotCore/job/BotCore5", "module.tar")]
 
@@ -16,7 +16,7 @@ namespace Bot.Assemble
     public class ASMInfo
     {
         public static string BotName = "ZBotCore";
-        public static string BotVer = "5.0.5.1250";
+        public static string BotVer = "5.0.5.1251";
         public static string GitPassword
         {
             get
