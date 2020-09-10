457c0c2e2861692787f4f082e4514911dead020a
Zontreck <tara@zontreck.dev>
2020-09-10 09:30:57 -0700

    update flags

--- Assembly/ASMInfo.cs
+++ Assembly/ASMInfo.cs
@@ -7,16 +7,16 @@ using LibZNI;
 [assembly: AssemblyCompany("ZNI")]
 [assembly: AssemblyAlgorithmId(System.Configuration.Assemblies.AssemblyHashAlgorithm.MD5)]
 [assembly: AssemblyCopyright("(C) 2020 Tara Piccari")]
-[assembly: AssemblyFileVersion("5.0.5.1253")]
+[assembly: AssemblyFileVersion("5.0.5.1254")]
 [assembly: AssemblyDescription("Second Life Bot - BotCore5")]
-[assembly: AutoUpdater("/job/ZBotCore/job/BotCore5", "module.tar")]
+[assembly: AutoUpdater("/job/Bot", "!os!.tar")]
 
 namespace Bot.Assemble
 {
     public class ASMInfo
     {
         public static string BotName = "ZBotCore";
-        public static string BotVer = "5.0.5.1253";
+        public static string BotVer = "5.0.5.1254";
         public static string GitPassword
         {
             get
